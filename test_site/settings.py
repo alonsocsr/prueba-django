938dff834ffd20c1c48d1e86a3e85833c138d00b
césar <cesaralonso99@gmail.com>
2024-08-22 18:19:28 -0400

    tailwindcss added

--- test_site/settings.py
+++ test_site/settings.py
@@ -68,7 +68,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-          os.path.join(PROJECT_DIR, "test_site/templates")
+          os.path.join(PROJECT_DIR, "test_site/templates"),
+          BASE_DIR / "templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -131,6 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 SITE_ID = 1
 LOGIN_URL = '/login/'
